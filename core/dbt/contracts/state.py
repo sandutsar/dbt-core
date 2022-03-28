0760673a3f74300f2be561f076bd6574df4db4a0
Sung Won Chung <sungwonchung3@gmail.com>
2022-03-28 12:59:41 -0500

    update state with version checks

--- core/dbt/contracts/state.py
+++ core/dbt/contracts/state.py
@@ -15,34 +15,38 @@ class PreviousState:
         self.sources: Optional[FreshnessExecutionResultArtifact] = None
         self.sources_current: Optional[FreshnessExecutionResultArtifact] = None
 
-        manifest_path = self.path / 'manifest.json'
+        manifest_path = self.path / "manifest.json"
         if manifest_path.exists() and manifest_path.is_file():
             try:
-                self.manifest = WritableManifest.read(str(manifest_path))
+                self.manifest = WritableManifest.read_and_check_versions(str(manifest_path))
             except IncompatibleSchemaException as exc:
                 exc.add_filename(str(manifest_path))
                 raise
 
-        results_path = self.path / 'run_results.json'
+        results_path = self.path / "run_results.json"
         if results_path.exists() and results_path.is_file():
             try:
-                self.results = RunResultsArtifact.read(str(results_path))
+                self.results = RunResultsArtifact.read_and_check_versions(str(results_path))
             except IncompatibleSchemaException as exc:
                 exc.add_filename(str(results_path))
                 raise
 
-        sources_path = self.path / 'sources.json'
+        sources_path = self.path / "sources.json"
         if sources_path.exists() and sources_path.is_file():
             try:
-                self.sources = FreshnessExecutionResultArtifact.read(str(sources_path))
+                self.sources = FreshnessExecutionResultArtifact.read_and_check_versions(
+                    str(sources_path)
+                )
             except IncompatibleSchemaException as exc:
                 exc.add_filename(str(sources_path))
                 raise
 
-        sources_current_path = self.current_path / 'sources.json'
+        sources_current_path = self.current_path / "sources.json"
         if sources_current_path.exists() and sources_current_path.is_file():
             try:
-                self.sources_current = FreshnessExecutionResultArtifact.read(str(sources_current_path))
+                self.sources_current = FreshnessExecutionResultArtifact.read_and_check_versions(
+                    str(sources_current_path)
+                )
             except IncompatibleSchemaException as exc:
                 exc.add_filename(str(sources_current_path))
                 raise
